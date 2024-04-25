129aa436aa97c861cfa1a958a7b150a73f34609a
muskanc123 <muskanchawla996@gmail.com>
2024-04-25 16:25:43 +0500

    Completed Calculator code

---
+++ Calculator using React/src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Calculator from './calculator.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Calculator/>
+)
